826fd9c47351a8681d62cea78408c52dd79ca954
luis <florezf90@gmail.com>
2023-10-21 09:01:04 -0500

    installed the inquirer package into the files, and added pseudo code in order to have a clear picture of what needed to be done in order to execute the app properly

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+let template = `
+
+`
+
+/// pseudociding
+
+// TODO: Install the inquirer package------------------------done 
+
+// TODO: import the inquirer and the FS methods---------------------done.
+
+// TODO: Create a variable that will hold the template for the professional readme file.---------------------done.
+
+// TODO: look for a good template and based on that create the template for your project.
+
+// TODO: in order to make the templade customizable, create the variable containing a literal by using the `` wrappers
+
+// TODO: use inquirer prompt([]) in order to get the user all the options you want them to be able to personalize.
+
+// TODO: Look again for tthe inquirer doc in order to refresh the questions types that we can prompt the user and based on that, see how m
+// many do you want to prompt.
+
+// TODO: once done, come back to the template and add each user prompt to it's corresponding location by using ${unitname}.
+
+// TODO:Then, with the template and the questions ready, use .then((response) =>  {}) in order to be able to generate a file qith the user prompts
+
+// TODO: create a var that will hold the user responses and use it within the .then function we will create.
+
