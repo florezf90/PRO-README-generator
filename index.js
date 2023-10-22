bdc4791b8da30b5b0f318bab13b76d2139f06d44
luis <florezf90@gmail.com>
2023-10-22 13:11:19 -0500

    finalized main readme for the project and inizialited the template for the generator

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const fs = require('fs');
 
 let template = `
 
+<p><h1 align= "center">${project.name}</h1></p>
+
+
+  # About
+
+${project.description}
+
+![Placeholder Image](${screenshot.placeholder}/400/400)
+
+
+
+# Built With 
+
+ List of any major frameworks/libraries used to design the project: 
+
+
+* [![NODE.JS](https://img.shields.io/badge/NODE.JS-green?style=flat&logo=node.js&logoColor=white&logoWidth=21&link=https://nodejs.org/en)](https://nodejs.org/en)
+
+* [![JavaScript](https://img.shields.io/badge/JavaScript-white?style=flat&logo=JavaScript&logoColor=yellow&logoWidth=21&link=https://www.w3schools.com/js/)](https://www.w3schools.com/js/)
+
+*  [![NPM INQUIRER](https://img.shields.io/badge/NPM%20INQUIRER-red?style=flat&logo=npm&logoColor=white&link=https://www.npmjs.com/package/inquirer)](https://www.npmjs.com/package/inquirer)
+
+
+
 `
 
 /// pseudociding
@@ -13,7 +37,7 @@ let template = `
 
 // TODO: Create a variable that will hold the template for the professional readme file.---------------------done.
 
-// TODO: look for a good template and based on that create the template for your project.
+// TODO: look for a good template and based on that create the template for your project.-----------
 
 // TODO: in order to make the templade customizable, create the variable containing a literal by using the `` wrappers
 
